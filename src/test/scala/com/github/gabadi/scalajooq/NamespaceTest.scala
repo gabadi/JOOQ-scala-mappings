26204597e30934358638accd0069bb6851fe5377
gabadi <gabriel.d.abadi@gmail.com>
2015-07-22 21:12:32 -0300

    tests simplification

--- src/test/scala/com/github/gabadi/scalajooq/NamespaceTest.scala
+++ src/test/scala/com/github/gabadi/scalajooq/NamespaceTest.scala
@@ -17,8 +17,6 @@ class NamespaceTest extends BaseSpec {
   "Namespace" when {
     "fields" should {
       "be only mapped" in DB.withRollback { dsl =>
-        implicit val NameMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Name]("profileName")
-        implicit val ProfileMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Profile]("profile")
         val Mapper = JooqMeta.metaOf[tables.FullUser, FullUserRecord, ProfileUser]
 
         Mapper.fields should contain theSameElementsAs FULL_USER.ID :: FULL_USER.PROFILE_NAME_FIRST :: FULL_USER.PROFILE_NAME_LAST :: Nil
@@ -38,54 +36,6 @@ class NamespaceTest extends BaseSpec {
         entity shouldBe Address("street", 1, "tel")
       }
 
-      "map namespace values nested" in DB.withRollback { dsl =>
-
-        val record = dsl.newRecord(USER_WITH_ADDRESS)
-        record.setAddressNumber(1l)
-        record.setAddressStreet("street")
-        record.setAddressTelephone("tel")
-        record.setId(2l)
-        record.setFirstName("name")
-        record.setLastName("last")
-
-        implicit val addressMapping = JooqMeta.namespacedMetaOf[tables.UserWithAddress, UserWithAddressRecord, Address]("address")
-        val mapper = JooqMeta.metaOf[tables.UserWithAddress, UserWithAddressRecord, UserWithAddress]
-
-        val entity = mapper.toEntity(record)
-        entity shouldBe UserWithAddress(2l, "name", "last", Address("street", 1l, "tel"))
-      }
-
-      "map namespace values nested option present" in DB.withRollback { dsl =>
-
-        val record = dsl.newRecord(USER_WITH_ADDRESS)
-        record.setAddressNumber(1l)
-        record.setAddressStreet("street")
-        record.setId(2l)
-        record.setFirstName("name")
-        record.setLastName("last")
-        record.setAddressTelephone("tel")
-
-        implicit val addressMapping = JooqMeta.namespacedMetaOf[tables.UserWithAddress, UserWithAddressRecord, Address]("address")
-        val mapper = JooqMeta.metaOf[tables.UserWithAddress, UserWithAddressRecord, UserWithOptAddress]
-
-        val entity = mapper.toEntity(record)
-        entity shouldBe UserWithOptAddress(2l, "name", "last", Some(Address("street", 1l, "tel")))
-      }
-
-      "map namespace values nested option absent" in DB.withRollback { dsl =>
-
-        val record = dsl.newRecord(USER_WITH_ADDRESS)
-        record.setId(2l)
-        record.setFirstName("name")
-        record.setLastName("last")
-
-        implicit val addressMapping = JooqMeta.namespacedMetaOf[tables.UserWithAddress, UserWithAddressRecord, Address](("address"))
-        val mapper = JooqMeta.metaOf[tables.UserWithAddress, UserWithAddressRecord, UserWithOptAddress]
-
-        val entity = mapper.toEntity(record)
-        entity shouldBe UserWithOptAddress(2l, "name", "last", None)
-      }
-
       "multi nesting" in DB.withRollback { dsl =>
 
         val record = dsl.newRecord(FULL_USER)
@@ -93,8 +43,6 @@ class NamespaceTest extends BaseSpec {
         record.setProfileNameFirst("name")
         record.setProfileNameLast("last")
 
-        implicit lazy val nameMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Name]("profileName")
-        implicit lazy val profileMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Profile]("profile")
         val mapper = JooqMeta.metaOf[tables.FullUser, FullUserRecord, ProfileUser]
 
         mapper.toEntity(record) shouldBe ProfileUser(1l, Profile(Name("name", "last")))
@@ -107,8 +55,6 @@ class NamespaceTest extends BaseSpec {
         record.setProfileNameFirst("name")
         record.setProfileNameLast("last")
 
-        implicit lazy val nameMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Name]("profileName")
-        implicit lazy val profileMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Profile]("profile")
         val mapper = JooqMeta.metaOf[tables.FullUser, FullUserRecord, ProfileUserOpt]
 
         mapper.toEntity(record) shouldBe ProfileUserOpt(1l, Some(Profile(Name("name", "last"))))
@@ -119,8 +65,6 @@ class NamespaceTest extends BaseSpec {
         val record = dsl.newRecord(FULL_USER)
         record.setId(1l)
 
-        implicit lazy val nameMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Name]("profileName")
-        implicit lazy val profileMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Profile]("profile")
         val mapper = JooqMeta.metaOf[tables.FullUser, FullUserRecord, ProfileUserOpt]
 
         mapper.toEntity(record) shouldBe ProfileUserOpt(1l, None)
@@ -161,7 +105,6 @@ class NamespaceTest extends BaseSpec {
 
       "map namespace values nested" in DB.withRollback { implicit dsl =>
 
-        implicit val addressMapping = JooqMeta.namespacedMetaOf[tables.UserWithAddress, UserWithAddressRecord, Address]("address")
         val mapper = JooqMeta.metaOf[tables.UserWithAddress, UserWithAddressRecord, UserWithAddress]
 
         val record = mapper.toRecord(UserWithAddress(2l, "name", "last", Address("street", 1l, "tel")))
@@ -173,37 +116,7 @@ class NamespaceTest extends BaseSpec {
         record.getAddressTelephone shouldBe "tel"
       }
 
-      "map namespace values nested option present" in DB.withRollback { implicit dsl =>
-
-        implicit val addressMapping = JooqMeta.namespacedMetaOf[tables.UserWithAddress, UserWithAddressRecord, Address]("address")
-        val mapper = JooqMeta.metaOf[tables.UserWithAddress, UserWithAddressRecord, UserWithOptAddress]
-        val record = mapper.toRecord(UserWithOptAddress(2l, "name", "last", Some(Address("street", 1l, "tel"))))
-
-        record.getId shouldBe 2l
-        record.getFirstName shouldBe "name"
-        record.getLastName shouldBe "last"
-        record.getAddressStreet shouldBe "street"
-        record.getAddressNumber shouldBe 1l
-        record.getAddressTelephone shouldBe "tel"
-      }
-
-      "map namespace values nested option absent" in DB.withRollback { implicit dsl =>
-
-        implicit val addressMapping = JooqMeta.namespacedMetaOf[tables.UserWithAddress, UserWithAddressRecord, Address]("address")
-        val mapper = JooqMeta.metaOf[tables.UserWithAddress, UserWithAddressRecord, UserWithOptAddress]
-        val record = mapper.toRecord(UserWithOptAddress(2l, "name", "last", None))
-
-        record.getId shouldBe 2l
-        record.getFirstName shouldBe "name"
-        record.getLastName shouldBe "last"
-        record.getAddressStreet shouldBe null
-        record.getAddressNumber shouldBe null
-        record.getAddressTelephone shouldBe null
-      }
-
       "multi nesting" in DB.withRollback { implicit dsl =>
-        implicit lazy val nameMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Name]("profileName")
-        implicit lazy val profileMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Profile]("profile")
         val mapper = JooqMeta.metaOf[tables.FullUser, FullUserRecord, ProfileUser]
         val record = mapper.toRecord(ProfileUser(1l, Profile(Name("name", "last"))))
 
@@ -213,8 +126,6 @@ class NamespaceTest extends BaseSpec {
       }
 
       "multi nesting opt present" in DB.withRollback { implicit dsl =>
-        implicit lazy val nameMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Name]("profileName")
-        implicit lazy val profileMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Profile]("profile")
         val mapper = JooqMeta.metaOf[tables.FullUser, FullUserRecord, ProfileUserOpt]
         val record = mapper.toRecord(ProfileUserOpt(1l, Some(Profile(Name("name", "last")))))
 
@@ -224,8 +135,6 @@ class NamespaceTest extends BaseSpec {
       }
 
       "multi nesting opt absent" in DB.withRollback { implicit dsl =>
-        implicit lazy val nameMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Name]("profileName")
-        implicit lazy val profileMapping = JooqMeta.namespacedMetaOf[tables.FullUser, FullUserRecord, Profile]("profile")
         val mapper = JooqMeta.metaOf[tables.FullUser, FullUserRecord, ProfileUserOpt]
         val record = mapper.toRecord(ProfileUserOpt(1l, None))
 
